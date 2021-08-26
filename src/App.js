5b1385dfa7543fa77017cb4763a3ff0653442d76
Wiltei <vilho.teivainen@gmail.com>
2021-08-26 12:04:01 +0300

    Ready for 1.2 step2.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+/* Refaktoroi vielä komponentti Content siten, että se ei itse renderöi yhdenkään osan nimeä eikä sen tehtävälukumäärää vaan ainoastaan kolme Part-nimistä komponenttia, joista kukin siis renderöi yhden osan nimen ja tehtävämäärän. */
+
 const App = (props) => {
   return (
     <div>
@@ -7,10 +9,9 @@ const App = (props) => {
         <Header course="Half Stack application development" />
       </h1>
       <p>
-        <Content part1="Fundamentals of React" exercises1="10" />
-
-        <Content part2="Using props to pass data" exercises2="7" />
-        <Content part3="State of a component" exercises3="14" />
+        <Content part="Fundamentals of React" exercises1={10} />
+        <Content part="Using props to pass data" exercises2={7} />
+        <Content part="State of a component" exercises3={14} />
       </p>
       <p>
         <Total total={props.exercises1 + props.exercises2 + props.exercises3} />
@@ -27,18 +28,22 @@ const Header = (props) => {
   );
 };
 
-const Content = (props) => {
+/* const Part = (props) => {
   return (
     <div>
       <p>
-        {props.part1} {props.exercises1}
+        <Content part="Fundamentals of React" exercises1={10} />
+        <Content part="Using props to pass data" exercises2={7} />
+        <Content part="State of a component" exercises3={14} />
       </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+    </div>
+  );
+}; */
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part}</p>
     </div>
   );
 };
